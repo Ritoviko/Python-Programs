d05562546b86b13333d1a3a89036ca24b38392a0
Ritoviko <95902980+Ritoviko@users.noreply.github.com>
2022-05-30 17:45:04 +0530

    Update binary search.py

--- binary search.py
+++ binary search.py
@@ -18,6 +18,14 @@ n = int(input())
 arr = []
 for i in range(n):
     arr.append(int(input()))
+
+for i in range(n):
+    for j in range(i):
+        if arr[j]>arr[i]:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+print(arr)
 x = int(input())
 y = binary(arr, x, n)
 print(y)
