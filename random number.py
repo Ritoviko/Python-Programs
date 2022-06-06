a77643554b9a8e5bcd26e8d1d0129101c61914f1
Ritoviko <95902980+Ritoviko@users.noreply.github.com>
2022-06-06 15:04:36 +0530

    Update random number.py

--- random number.py
+++ random number.py
@@ -14,49 +14,50 @@ attempt = 0
 choice = input("Do you want to Play? (yes/no): ")
 print("------------------------------")
 num = int(random.randint(1, 10))
-while choice.lower() == "yes":
-    guess = int(input("Guess a number between 1 to 10: "))
-    print("------------------------------")
-
-    if guess<1 or guess>10:
-        print("Choose the number within the range.")
+if choice.lower()=="yes":
+    while True:
+        guess = int(input("Guess a number between 1 to 10: "))
         print("------------------------------")
 
-    elif guess>num:
-        print("Hint: its lower")
-        print("------------------------------")
-        attempt +=1
-    elif guess<num:
-        print("Hint: its higher")
-        print("------------------------------")
-        attempt +=1
-    elif guess==num:
-        print("Correct guess")
-        print("It was ", num)
-        print("------------------------------")
-        attempt+=1
-        a_list.append(attempt)
-        
-        print("You Got it in ", attempt, " attempts.")
-        print("------------------------------")
-        opt = input("Do you wanna Play again?")
-        if opt.lower()=="yes":
-            attempt = 0
-            num = int(random.randint(1, 10))
-        else:
-            print("Ok")
+        if guess<1 or guess>10:
+            print("Choose the number within the range.")
             print("------------------------------")
-            opt2 = input("Do you want to see the highscore so far??")
-            if opt2.lower()=="yes":
-                show_score()
-                print("------------------------------")
+
+        elif guess>num:
+            print("Hint: its lower")
+            print("------------------------------")
+            attempt +=1
+        elif guess<num:
+            print("Hint: its higher")
+            print("------------------------------")
+            attempt +=1
+        elif guess==num:
+            print("Correct guess")
+            print("It was ", num)
+            print("------------------------------")
+            attempt+=1
+            a_list.append(attempt)
+        
+            print("You Got it in ", attempt, " attempts.")
+            print("------------------------------")
+            opt = input("Do you wanna Play again?")
+            if opt.lower()=="yes":
+                attempt = 0
+                num = int(random.randint(1, 10))
             else:
-                print("Have a good day")
+                print("Ok")
                 print("------------------------------")
-                break
+                opt2 = input("Do you want to see the highscore so far??")
+                if opt2.lower()=="yes":
+                    show_score()
+                    print("------------------------------")
+                    break
+                else:
+                    print("Have a good day")
+                    print("------------------------------")
+                    break
             
-    else:
-        print("Ok, that's cool")
-        print("------------------------------")
-        break
+else:
+    print("Ok, that's cool")
+    print("------------------------------")
 
